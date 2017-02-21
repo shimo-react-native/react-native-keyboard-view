f22e86defa5edaf72cf619721ad2470b26935000
Horcrux <master@horcrux.cn>
2017-02-21 16:07:24 +0800

    Add js callback

--- index.ios.js
+++ index.ios.js
@@ -1,5 +1,5 @@
 import React, { Component, PropTypes } from 'react';
-import { NativeModules, StyleSheet, findNodeHandle, View,
+import { NativeModules, StyleSheet, findNodeHandle, View, Keyboard,
     requireNativeComponent } from 'react-native';
 
 const styles = StyleSheet.create({
@@ -30,6 +30,19 @@ export default class extends Component {
         onKeyboardChanged: PropTypes.func
     };
 
+    componentWillMount() {
+        this._willShow = this._willShow.bind(this);
+        this._didHide = this._didHide.bind(this);
+        this._willChangeFrame = this._willChangeFrame.bind(this);
+
+        Keyboard.addListener('keyboardWillShow', this._willShow);
+        Keyboard.addListener('keyboardDidHide', this._didHide);
+        Keyboard.addListener('keyboardWillChangeFrame', this._willChangeFrame);
+    }
+
+    _visible;
+    _active;
+
     open() {
         this._callKeyboardService('openKeyboard');
     }
@@ -40,18 +53,40 @@ export default class extends Component {
 
     showKeyboard() {
         this._callKeyboardService('showKeyboard');
+        this._visible = true;
     }
 
     hideKeyboard() {
         this._callKeyboardService('hideKeyboard');
+        this._visible = false;
     }
 
     toggleKeyboard() {
         this._callKeyboardService('toggleKeyboard');
+        this._visible = !this._visible;
     }
 
     _callKeyboardService(method) {
         return NativeModules.RNKeyboardViewManager[method](findNodeHandle(this.refs.keyboardView));
+    }
+
+    _willShow({ endCoordinates: { height } }) {
+        this._active = true;
+        const { onShow } = this.props;
+        onShow && onShow(false, height);
+    }
+
+    _didHide() {
+        this._active = false;
+        const { onHide } = this.props;
+        onHide && onHide(this._visible);
+    }
+
+    _willChangeFrame({ endCoordinates: { height } }) {
+        if (this._active) {
+            const { onKeyboardChanged } = this.props;
+            onKeyboardChanged && onKeyboardChanged(this._visible, height);
+        }
     }
 
     render() {
