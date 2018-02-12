c3fc63dfa6d875bc687307b59bb4f7917ebc2a9c
song <wusong@shimo.im>
2018-02-12 17:04:34 +0800

    fix:keyboard can be click when it's gone

--- android/src/main/java/im/shimo/react/keyboard/KeyboardCoverView.java
+++ android/src/main/java/im/shimo/react/keyboard/KeyboardCoverView.java
@@ -2,12 +2,16 @@ package im.shimo.react.keyboard;
 
 
 import android.content.Context;
+import android.support.annotation.NonNull;
 import android.view.MotionEvent;
+import android.view.View;
 
 import com.facebook.react.uimanager.PointerEvents;
 import com.facebook.react.views.view.ReactViewGroup;
 
 public class KeyboardCoverView extends ReactViewGroup {
+    private volatile PointerEvents pointerEvents = PointerEvents.BOX_NONE;
+
     public KeyboardCoverView(Context context) {
         super(context);
     }
@@ -15,12 +19,22 @@ public class KeyboardCoverView extends ReactViewGroup {
     @Override
     public PointerEvents getPointerEvents() {
         // Override getPointerEvents or it will return PointerEvents.AUTO.
-        return PointerEvents.BOX_NONE;
+        return pointerEvents;
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         // Override onTouchEvent or it will return true.
         return false;
+    }
+
+    @Override
+    protected void onVisibilityChanged(@NonNull View changedView, int visibility) {
+        super.onVisibilityChanged(changedView, visibility);
+        if (visibility == VISIBLE) {
+            pointerEvents = PointerEvents.BOX_NONE;
+        } else {
+            pointerEvents = PointerEvents.NONE;
+        }
     }
 }
